c4cf2af2dc77b58175ad69b3bf9e367f770b0946
KST <kimsangtae.me@gmail.com>
2020-01-23 23:35:28 +0900

    반복문으로 100까지 덧셈숫자 구하기

--- src/test1/main1.java
+++ src/test1/main1.java
@@ -3,7 +3,12 @@ package test1;
 public class main1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int sum = 0;
+		
+		for(int i=1; i <= 100; i++) {
+			sum += i;
+		}
+		System.out.println(sum);
 
 	}
 
